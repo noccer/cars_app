1b1bfa2028fcc67b4214efc93923a145df7214b2
Niall O'Callaghan <noccer@noccer-MacBook.local>
2016-05-24 08:30:13 +1000

    added column headings for database table

--- db/migrate/20160523222022_create_cars.rb
+++ db/migrate/20160523222022_create_cars.rb
@@ -1,7 +1,10 @@
 class CreateCars < ActiveRecord::Migration
   def change
     create_table :cars do |t|
-
+		t.string :make
+		t.string :model
+		t.integer :year
+		t.integer :mileage
       t.timestamps null: false
     end
   end
