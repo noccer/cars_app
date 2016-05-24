05609e840861fe11333cfad7e6dbdb613cf8f742
Niall O'Callaghan <noccer@noccer-MacBook.local>
2016-05-25 09:25:05 +1000

    add in the generate route: get '/generate' => 'home#make_car'

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ root 'home#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get '/generate' => 'home#make_car'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
